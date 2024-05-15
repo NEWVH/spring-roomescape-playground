d4c6956c32b0645a6ac72874f8eaf7561d4615db
hz <bxxxbhy@gmail.com>
2024-05-15 20:17:22 +0900

    step3-2. 예약 취소

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.Matchers.is;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 
 public class MissionStepTest {
+
     @Test
     void 삼단계_예약_추가() {
         Map<String, String> params = new HashMap<>();
@@ -30,22 +31,34 @@ public class MissionStepTest {
                 .header("Location", "/reservations/1")
                 .body("id", is(1));
 
-//        RestAssured.given()
-//                .log().all()
-//                .when().get("/reservations")
-//                .then().log().all()
-//                .statusCode(200).body("size()", is(1));
-
+       RestAssured.given()
+              .log().all()
+              .when().get("/reservations")
+              .then().log().all()
+              .statusCode(200).body("size()", is(1));
     }
 
         @Test
-        void 삼단계_예약_삭제() {
+        void 삼단계_예약_취소() {
+            Map<String, String> params = new HashMap<>();
+            params.put("name", "브라운");
+            params.put("date", "2023-08-05");
+            params.put("time", "15:40");
+
+            RestAssured.given()
+                    .log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all().statusCode(201)
+                    .header("Location", "/reservations/1")
+                    .body("id", is(1));
+
             RestAssured.given()
                     .log().all()
                     .when().delete("/reservations/1")
                     .then().log().all().statusCode(204);
 
             RestAssured.given().log().all().when().get("/reservations").then().log().all().statusCode(200).body("size()", is(0));
-
         }
 }
